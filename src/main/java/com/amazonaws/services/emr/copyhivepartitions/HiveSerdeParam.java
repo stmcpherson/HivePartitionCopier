32a25d139110c6970b5362ad6e3e697d4da7e35d
Mandus Momberg <mombergm@amazon.com>
2015-01-25 21:26:26 +0000

    Adding String Escape Function

--- src/main/java/com/amazonaws/services/emr/copyhivepartitions/HiveSerdeParam.java
+++ src/main/java/com/amazonaws/services/emr/copyhivepartitions/HiveSerdeParam.java
@@ -29,8 +29,26 @@ public class HiveSerdeParam {
     if(!isFirst){
       sqlout+= ",";
     }
-    sqlout += "(" +SERDE_ID+ ", '" +PARAM_KEY+ "', '"+PARAM_VALUE+"')"; 
+    sqlout += "(" + SERDE_ID + ", '" +PARAM_KEY+ "', '"+ getMysqlRealScapeString( PARAM_VALUE )+"')"; 
     return sqlout; 
   }
+  
+  public static synchronized String getMysqlRealScapeString(String str) {
+	  String data = null;
+	  if (str != null && str.length() > 0) {
+	   str = str.replace("\\", "\\\\");
+	   str = str.replace("'", "\\'");
+	   str = str.replace("\0", "\\0");
+	   str = str.replace("\1", "\\1");
+	   str = str.replace("\n", "\\n");
+	   str = str.replace("\r", "\\r");
+	   str = str.replace("\"", "\\\"");
+	   str = str.replace("\\x1a", "\\Z");
+	   
+	   data = str;
+	  }
+	  return data;
+	 }
 
 }
+
